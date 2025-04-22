61e1756544a25003fb46d59bd6e8115f62fab76e
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2025-04-21 23:33:16 -0300

    feat: Changin basic repo style

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const menuBtn = document.getElementById("menu-btn");
 
 const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = menuBtn.querySelectorAll(".line")
-
+const menuBtnIcon = menuBtn.querySelectorAll(".line");
+const dateInput = document.getElementById("date");
 
 function changeBtn(isOpen) {
   menuBtnIcon[0].style.transform = isOpen ? "rotate(135deg) translate(5px, -5px)" : "rotate(0deg) translate(0)";
@@ -10,50 +10,73 @@ function changeBtn(isOpen) {
   menuBtnIcon[2].style.transform = isOpen ? "rotate(-135deg) translate(5px, 5px)" : "rotate(0deg) translate(0)";
 }
 
-menuBtn.addEventListener("click", (e) => {
+menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   const isOpen = navLinks.classList.contains("open");
   changeBtn(isOpen);
 })
 
-navLinks.addEventListener("click", (e) => {
+navLinks.addEventListener("click", () => {
   navLinks.classList.remove("open");
   changeBtn(false)
 })
 
-const scrollRevealOption = {
-  distance: "50px",
-  origin: "bottom",
-  duration: 1000
-}
+dateInput.addEventListener("input", (e) => {
 
-ScrollReveal().reveal(".header_image img", {
-  ...scrollRevealOption,
-  orgin: "right"
-})
+});
 
-ScrollReveal().reveal(".header_content h1", {
-  ...scrollRevealOption,
-  delay: 500
-})
+dateInput.placeholder = Intl.DateTimeFormat("pt-BR", {
+  dateStyle: "medium"
+}).format(new Date())
 
-ScrollReveal().reveal(".header_content p", {
-  ...scrollRevealOption,
-  delay: 1000
-})
 
-ScrollReveal().reveal(".header_content form", {
-  ...scrollRevealOption,
-  delay: 1500
-})
+ScrollReveal().reveal('.header_image_card-1', {
+  delay: 375,
+  scale: 1.5,
+  distance: "15px"
+});
 
-ScrollReveal().reveal(".header_content .bar", {
-  ...scrollRevealOption,
-  delay: 2000
-})
+ScrollReveal().reveal('.header_image_card-3', {
+  delay: 300,
+  scale: 1.5,
+  distance: "15px"
+});
 
-ScrollReveal().reveal(".header_image_card", {
-  duration: 1000,
-  interval: 500,
-  delay: 2500,
-})
+ScrollReveal().reveal('.header_image_card-5', {
+  delay: 450,
+  scale: 1.5,
+  distance: "20px"
+});
+
+ScrollReveal().reveal('.header_image_card-2', {
+  delay: 600,
+  scale: 1.5,
+  distance: "20px"
+});
+
+ScrollReveal().reveal('.header_image_card-4', {
+  delay: 500,
+  scale: 1.5,
+  distance: "20px"
+});
+
+ScrollReveal().reveal('.header_image_card-6', {
+  delay: 700,
+  scale: 1.5,
+  distance: "20px"
+});
+
+
+ScrollReveal().reveal('.header_content h1', {
+  delay: 250,
+  distance: "50px"
+});
+
+ScrollReveal().reveal('.header_content p', {
+  delay: 350,
+  distance: "50px"
+});
+ScrollReveal().reveal('form', {
+  delay: 750,
+  distance: "50px"
+});
